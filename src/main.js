f657943f6c413affd916357dedbde5baa3a74a6c
alexsreberni <alexsrebernic@gmail.com>
2022-07-24 21:19:50 -0300

    add router files, add principals views, add navbar and toast

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-
-createApp(App).mount('#app')
+import router from "./router.js";
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './presentation/home/view/Home.vue'
+import Documentation from './presentation/documentation/view/Documentation.vue'
+import About from './presentation/about/view/About.vue'
+import Contact from './presentation/contact/view/Contact.vue'
+import NotFound from './presentation/notfound/view/NotFound.vue'
+const routes = [
+    {
+        path: "/home",
+        meta: { title: "Home" },
+        component: Home,
+    },
+    {
+        path: "/documentation",
+        meta: { title: "Documentation" },
+        component: Documentation,
+    },
+    {
+        path: "/about",
+        meta: { title: "About" },
+        component: About,
+    },
+    {
+        path: "/contact",
+        meta: { title: "Contact" },
+        component: Contact,
+    },
+    { 
+        path: "/:path(.*)", 
+        meta: { title: "NotFound" },
+        component: NotFound 
+    },
+
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router
