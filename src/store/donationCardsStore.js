a1c1202956120f54b2ff820ac94383cbf654933d
alexsreberni <alexsrebernic@gmail.com>
2022-11-14 11:57:49 -0300

    move controller functions to store

--- src/store/donationCardsStore.js
+++ src/store/donationCardsStore.js
@@ -1,10 +1,13 @@
 import { defineStore } from 'pinia'
-
+import { setDoc,doc,collection,addDoc,deleteDoc, Timestamp, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import {db} from "../../firebase.config"
+const getDocRefFromNetworkTestnet = (network,address) =>  doc(db,network,address)
+const getNetworkRefCollection = (network) => collection(db,network)
 export const useDonationsCardsStore = defineStore('donations_cards', {
     state: () => (
         { 
         "Binance-testnet": [],
-        "Rinkeby":[] 
+        "Goerli":[] 
         }
         ),
     getters: {
@@ -21,4 +24,85 @@ export const useDonationsCardsStore = defineStore('donations_cards', {
             this[network] = this[network].filter(card => card.owner != owner_address)
         }
     },
+    actions: {
+        createCan(network,dataCan){
+            return new Promise(async (resolve,reject) => {
+                try {
+                    const {first_name,last_name,avatar_color,details,address} = dataCan
+                    if(await getCan(network,address)) reject("Address already have a can created")
+                    const can = {
+                        first_name,
+                        last_name,
+                        avatar_color,
+                        details,
+                        donors: [],
+                        createdAt: Timestamp.now(),
+                        total_balance: 0,
+                        current_balance: 0,
+                    }
+                    await setDoc(getDocRefFromNetworkTestnet(network,address),can)
+                    this[network].push(can);
+                    resolve(true)
+                } catch ( error ){
+                    reject(error)
+                }
+            })
+        },
+        deleteCan(network,address){
+            return new Promise(async (resolve,reject) => {
+                try {
+                    await deleteDoc(getDocRefFromNetworkTestnet(network,address))
+                    resolve(true)
+                } catch( error ){
+                    reject(error)
+                }
+            })
+        },
+        getCan(network,address){
+            return new Promise(async(resolve,reject) => {
+                try {
+                    const can = (await getDoc(getDocRefFromNetworkTestnet(network,address)))
+                    if(!can.exists){
+                        resolve(null)
+                    } else {
+                        resolve(can.data())
+                    }
+                } catch(error){
+                    reject(error)
+                }
+            })
+        },
+        getCansFromNetwork(network){
+            return new Promise(async(resolve,reject) => {
+                try {
+                    const cans = (await getDocs(getNetworkRefCollection(network)))
+                    if(cans.size == 0){
+                        resolve(null)
+                    } else {
+                        resolve(cans.docs)
+                    }
+                } catch(error){
+                    reject(error)
+                }
+            })
+        },
+        donateToCan(network,can,donor){
+            return new Promise(async (resolve,reject) => {
+                try {
+                
+                    const newTotalBalanceCan = Number(can.total_balance) + Number(donor.amountDonated);
+                    const newCurrentBalanceCan = Number(can.current_balance) + Number(donor.amountDonated);
+                    const newArrayOfDonors = [...can.donors, donor.address]
+                    await updateDoc(getDocRefFromNetworkTestnet(network,can.address),{
+                        total_balance : newTotalBalanceCan,
+                        current_balance : newCurrentBalanceCan,
+                        donors: newArrayOfDonors,
+                    })
+                    resolve(true)
+                } catch(error){
+                    reject(error)
+                }
+            })
+        }
+    }
 })
